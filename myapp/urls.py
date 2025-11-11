63f1382678916da62156976944919c949b3b1061
Omar Velazquez <ovelazquez.mx@gmail.com>
2025-11-11 16:49:42 -0600

    params for urls/paths and administrator section

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
     path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
-def hello(request):
-    return HttpResponse("<h2>Hello World!</h2>")
+def index(request):
+    return HttpResponse("<h1>Index Page</h1>")
+
+def hello(request, username):
+    print(f"Username received: {username}")
+    return HttpResponse("<h2>Hello %s</h2>" % username)
 
 def about(request):
     return HttpResponse("<h2>About Page</h2>")
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    #task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return JsonResponse("Task: %s" % task.title, safe=False)
