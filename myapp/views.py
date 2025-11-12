8d8d26062d1e71f6dbac47fede1da25a26706c9b
Omar Velazquez <ovelazquez.mx@gmail.com>
2025-11-11 18:14:59 -0600

    Jinja loops

--- myapp/views.py
+++ myapp/views.py
@@ -28,4 +28,7 @@ def projects(request):
 def tasks(request):
     #task = Task.objects.get(id=id)
     #task = get_object_or_404(Task, id=id)
-    return render(request, "tasks.html")
+    tasks = Task.objects.all()
+    return render(request, "tasks.html",{
+        "tasks": tasks
+    })
